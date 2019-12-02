113e11116e0ecf17a8817ad87819b4aa7e521373
francisco gonzalez <fcojonathan@gmail.com>
2019-12-02 00:23:04 -0600

    Improve uuid field generation

--- api/utils/common.py
+++
@@ -1,5 +0,0 @@
-import uuid
-
-
-def make_uuid(*args, **kwargs):
-    return str(uuid.uuid4()).replace("-", "")
--- api/utils/models.py
+++ api/utils/models.py
@@ -1,19 +1,11 @@
+import uuid
 from django.db import models
-
-from utils.common import make_uuid
 
 
 class BaseModel(models.Model):
-    uuid = models.CharField(db_index=True, max_length=32, unique=True)
+    uuid = models.UUIDField(db_index=True, default=uuid.uuid4, editable=False)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
     class Meta:
         abstract = True
-
-    def save(self, *args, **kwargs):
-        if not self.uuid:
-            self.uuid = make_uuid()
-            while type(self).objects.filter(uuid=self.uuid).exists():
-                self.uuid = make_uuid()
-        return super(BaseModel, self).save(*args, **kwargs)
